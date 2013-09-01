3f1e1e7db6bd31137dcea306299bf9ef6e198d59
Stuart Nelson <stuartnelson3@gmail.com>
2013-09-01 12:42:38 -0400

    refactored update() into separate methods in same file

--- client/client.js
+++ client/client.js
@@ -27,6 +27,7 @@ Template.beerList.events({
     var id = event.target.getAttribute('data-id');
 
     Meteor.call('removeBeer', id);
+    update();
   },
 
   'click .js-removeVote': function(event, template) {
@@ -57,34 +58,46 @@ var update = function() {
                 .attr('width', 960)
                 .attr('height', 20 * beers.length);
 
-  var x = d3.scale.linear()
-            .domain([0, d3.max(votes)])
-            .range([0, 480]);
-
-  // make me work with attr('y') below
-  var y = d3.scale.ordinal()
-            .domain([0,1,2,3])
-            .rangeBands([0, 120]);
-
   var bar_data = chart.selectAll('rect').data(votes);
+  var chart_text = chart.selectAll('text').data(beers);
 
-  bar_data.transition().duration(750)
-  .attr('width', function(d) {return x(d)});
+  transitionBarData(bar_data, xScale(votes));
+  transitionText(chart_text, xScale(votes));
 
-  bar_data
-       .enter().append('rect')
-       .attr('y', function(d, i) { return i * 20; })
-       .attr('width', x) // relative length
-       .attr('height', 20);
+};
 
+var transitionBarData = function(barData, xScale) {
+  barData.enter().append('rect')
+        .attr('y', function(d, i) { return i * 20; })
+        .attr('width', xScale) // relative length
+        .attr('height', 20);
 
-  chart.selectAll('text')
-       .data(beers)
+  barData.transition().duration(750)
+        .attr('width', xScale);
+
+  barData.exit()
+        .transition().duration(750)
+        .attr('width', xScale)
+        .remove();
+};
+
+var transitionText = function(chartText, xScale) {
+  chartText.transition().duration(750)
+        .attr('x', function(d) {return xScale(d.votes);})
+        .text(function(d) {return d.name+': '+d.votes+' votes'});
+
+  chartText
        .enter().append('text')
-       .attr('x', function(d,i) {return x(d.votes);})
+       .attr('x', function(d,i) {return xScale(d.votes);})
        .attr('y', function(d,i) { return i*20+10;})
        .attr('dx', -3) // padding-right
        .attr('dy', '.35em') // vertical-align: middle
        .attr('text-anchor', 'end') // text-align: right
        .text(function(d) { return d.name + ': ' + d.votes + ' votes'; });
+};
+
+var xScale = function(domain) {
+  return  d3.scale.linear()
+            .domain([0, d3.max(domain)])
+            .range([0, 480]);
 };
