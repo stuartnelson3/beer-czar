e6f7fe59d72f18f16cc61d130e7fb0f5b6379622
Stuart Nelson <stuartnelson3@gmail.com>
2013-09-01 10:02:49 -0400

    removed unnecessary x2 scale function

--- client/client.js
+++ client/client.js
@@ -59,25 +59,22 @@ Template.graph.rendered = function() {
 
   // samesies
   var y = d3.scale.ordinal()
-            .domain(votes)
+            .domain([0,1,2,3])
             .rangeBands([0, 120]);
 
   chart.selectAll("rect")
        .data(votes)
        .enter().append("rect")
        .attr("y", function(d, i) { return i * 20; })
+       // .attr("y", y)
        .attr("width", x) // relative length
        // .attr("width", function(d) {return d*100;}) // absolute length
        .attr("height", 20);
 
-  var x2 = d3.scale.linear()
-            .domain([0, d3.max(votes)])
-            .range([0, 480]);
-
   chart.selectAll("text")
        .data(votes)
        .enter().append("text")
-       .attr("x", x2)
+       .attr("x", x)
        .attr("y", function(d,i) { return i*20+10;})
        .attr("dx", -3) // padding-right
        .attr("dy", ".35em") // vertical-align: middle
