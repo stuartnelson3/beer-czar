f93cf146cf74757b19ffe5f7a950845583a7da35
Stuart Nelson <stuartnelson3@gmail.com>
2013-09-04 09:45:16 -0400

    escaped period in email check regex

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 Accounts.validateNewUser(function(user) {
   // beef up this regex?
-  if (user['services']['google']['email'].search(/@neo.com$/) > 0)
+  if (user['services']['google']['email'].search(/@neo\.com$/) > 0)
     return true;
   throw new Meteor.Error(403, 'You need to have a neo.com email address!');
 });
