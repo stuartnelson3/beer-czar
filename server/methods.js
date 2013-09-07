9514de9766018669ca9bb883b00f94f4dd81e891
Stuart Nelson <stuartnelson3@gmail.com>
2013-09-07 16:05:27 -0400

    attempt at adding some structure to this project

--- server/methods.js
+++ server/methods.js
@@ -1,17 +1,3 @@
-Accounts.validateNewUser(function(user) {
-  // beef up this regex?
-  if (user['services']['google']['email'].search(/@neo\.com$/) > 0)
-    return true;
-  throw new Meteor.Error(403, 'You need to have a neo.com email address!');
-});
-
-Accounts.onCreateUser(function(options, user) {
-  user.profile = options.profile;
-  user.profile['voteCount'] = 3;
-
-  return user;
-});
-
 Meteor.methods({
   addBeer: function(name) {
     if (name.length && !Beer.find({name:name}).count()) {
