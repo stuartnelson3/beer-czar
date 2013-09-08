4f4c9997c4fb63dc70bfd93b9d85410ef041d0e4
Stuart Nelson <stuartnelson3@gmail.com>
2013-09-08 16:52:37 -0400

    got rid of circles and added pie chart. created function to map users to data structure for pie chart.

--- client/pieChart.js
+++ client/pieChart.js
@@ -2,46 +2,97 @@ PieChart = function() {
 
   var render = function(updateFunction) {
     var users = Meteor.users.find().fetch();
-    // var users = Meteor.users.find({'profile.voteCount':{$gt:0}});
+    var totalUsers = users.count;
+
+    console.log(pcStructure(mappedUsers(users)));
+
     var chartSize = 480;
-
-    var chart = d3.select('svg.js-piechart')
-    .attr('class', 'chart')
-    .attr('width', chartSize)
-    .attr('height', chartSize);
-
-    var circleData = chart.selectAll('circle').data(users);
-
-    var red = d3.rgb(255, 0, 0);
-    var green = d3.rgb(0, 255, 0);
-
-    circleData.enter().append('circle')
-      .attr('r', function(user) { return 0; })
-      .attr('fill', red)
-      .attr('cy', 0)
-      .attr('cx', function(d,i) { return 20 * (i+1); })
-
-    circleData.transition().duration(2500)
-      .delay(function(d,i) { return i * 1000; })
-      .attr('r', function(user) { return 20; })
-      .attr('fill', green.darker(2))
-      .attr('cy', 80)
-      .attr('cx', function(d,i) { return 80 * (i+1); })
-    // var chart_text = chart.selectAll('text').data(beers);
-
     var r = 100;
     var p = Math.PI*r;
     var arc = d3.svg.arc()
-                .innerRadius(r-2)
-                .outerRadius(r)
-                .startAngle(0)
-                .endAngle(2*Math.PI)
+                .innerRadius(function(d,i) {
+                  return 0;
+                }).outerRadius(function(d,i) {
+                  return 85;
+                });
 
-    chart.append('path')
-      .attr('transform', 'translate(100,100)')
-      .attr('d', arc);
+    var indentReset;
+    var totalUsers = 10;
+    var offset = 0;
+
+    var data = [ // mock out aggregated user data
+      { voteCount: 3 }, { voteCount: 2 }, { voteCount: 4 }, { voteCount: 1 }
+    ];
+
+    createPieChart(data, arc, chartSize);
 
   };
+
+  var createPieChart = function(data, arc, size) {
+    var svg = d3.select('svg.js-piechart')
+    .attr('width', size)
+    .attr('height', size)
+    .append('g')
+    .attr('transform', 'translate(100,100)');
+
+    var pie = d3.layout.pie()
+        .value(function(d) { return d.voteCount; });
+
+    var color = d3.scale.ordinal()
+    .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
+
+    var g = svg.selectAll('.arc')
+      .data(pie(data))
+      .enter().append('g')
+      .attr('class', 'arc');
+
+      g.append('path')
+      .attr('d', arc)
+      .style('fill', function(d) { return color(d.data.voteCount); });
+
+      g.append('text')
+      .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')'; })
+      .attr('dy', '.35em')
+      .style('text-anchor', 'middle')
+      .text(function(d) { return d.data.voteCount; });
+  };
+
+  var pcStructure = function(users) {
+    var structure = [];
+    var zeroVote = {voteCount: 0, names: [], userCount: 0};
+    var oneVote = {voteCount: 1, names: [], userCount: 0};
+    var twoVote = {voteCount: 2, names: [], userCount: 0};
+    var threeVote = {voteCount: 3, names: [], userCount: 0};
+
+    structure.push(zeroVote);
+    structure.push(oneVote);
+    structure.push(twoVote);
+    structure.push(threeVote);
+
+    var hashMap = {
+      0: zeroVote,
+      1: oneVote,
+      2: twoVote,
+      3: threeVote
+    }
+
+    for (var i = 0, len = users.length; i < len; i++) {
+      var user = users[i];
+      var voteBucket = hashMap[user.voteCount];
+      if (voteBucket) {
+        voteBucket.names.push(user.name);
+        voteBucket.userCount += 1;
+      }
+    }
+
+    return structure;
+  };
+
+  var mappedUsers = function(users) {
+    return users.map(function(user) {
+      return {voteCount: user.profile.voteCount, name: user.profile.name};
+    });
+  }
 
   var initial = function() {};
   var update = function() {};
